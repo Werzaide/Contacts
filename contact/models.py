d1451b8b0ea36b8a80459daa7b78dda3aa5cfae7
Werzaide <werzaide@gmail.com>
2025-05-15 05:30:19 +0100

    Script to create contacts and CSS

--- contact/models.py
+++ contact/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # id (primary key)
 # first_name (string), last_name (string), phone (string)
@@ -8,6 +9,10 @@ from django.utils import timezone
 # picture (image)
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
     
     def __str__(self) -> str:
@@ -23,6 +28,7 @@ class Contact(models.Model):
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True)
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
