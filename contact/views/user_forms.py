e390d9ec04f853172eea208db808407cea797643
Werzaide <werzaide@gmail.com>
2025-05-19 02:33:57 +0100

    Check if user is logged in and form to update user Data

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.forms import AuthenticationForm
 
-from contact.forms import RegisterForm
+from contact.forms import RegisterForm, RegisterUpdateForm
 
 def register(request):
     form = RegisterForm()
@@ -45,6 +45,35 @@ def login_view(request):
             'form': form,
         }
     )
+
+
+def user_update(request):
+    form = RegisterUpdateForm(instance=request.user)
+    
+    if request.method != 'POST':
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form,
+            }
+        )
+    
+    form = RegisterUpdateForm(data=request.POST, instance=request.user)
+
+    if not form.is_valid():
+        return render(
+            request,
+            'contact/user_update.html',
+            {
+                'form': form,
+            }
+        )  
+      
+    form.save()
+
+    return redirect('contact:login')
+
 
 def logout_view(request):
     auth.logout(request)
